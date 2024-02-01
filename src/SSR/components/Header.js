0c35c8fed344286b6fdb8503e84d116a9c1eb4e6
samsoucoupe <samybendhiab@gmail.com>
2024-02-01 17:45:02 +0100

    #11 add images and scorrs

--- src/SSR/components/Header.js
+++ src/SSR/components/Header.js
@@ -1,15 +1,24 @@
-export const Header =
-    `<header class="flex justify-center items-center">
+export const Header = `
+    <header class="flex justify-center items-center">
         <nav x-data class="flex justify-between items-center container px-4">
             <div class="basis-1/4">
                 <a x-on:click="$store.pages.set('home')" class="cursor-pointer"><img src="/assets/images/Logo-WC-Studio.png" alt="Logo WC Studio"></a>
             </div>
             <div class="basis-2/4">
                 <h1 class="text-center mt-3">WC STUDIO</h1>
+            </div> 
+            <div class="basis-1/4 flex justify-end gap-3 ">
+                <div class="relative">
+                    <a x-on:click="$store.pages.set('shop')" class="cursor-pointer"><img src="/assets/images/Shop.png" alt="SHOP" class="hover:invert"></a>
+                    <div class="h-0">
+                        <span class="money font-bold relative " id="money">500</span>
+                        <img class="money relative " src="/assets/images/Money.png" alt="Logo money"></a>
+                    </div>
+                </div>
+               
+                <a x-on:click="$store.pages.set('account')" class="cursor-pointer"><img src="/assets/images/User.png" alt="Account" class="hover:invert"></a>
             </div>
-            <div class="basis-1/4 flex justify-end gap-2">
-                <a x-on:click="$store.pages.set('shop')" class="cursor-pointer">SHOP</a>
-                <a x-on:click="$store.pages.set('account')" class="cursor-pointer">ACCOUNT</a>
-            </div>
+            
+    
         </nav>
     </header>`
