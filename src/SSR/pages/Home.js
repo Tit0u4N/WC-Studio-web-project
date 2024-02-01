32bc2fbab8fafb94e2a0c3417ef329383a4f824f
Titouan Lacombe--Fabre <titouan.lacombe---fabre@etu.univ-cotedazur.fr>
2024-02-01 23:59:16 +0100

    fix: Suppression de teste sur la home page #20

--- src/SSR/pages/Home.js
+++ src/SSR/pages/Home.js
@@ -1,19 +1,4 @@
 export const Home =
     `<div x-data x-show="$store.pages.isShowing('home')">
         <h1>HOME</h1>
-        
-        <div x-data="{
-         game: '',
-         fetchGame: function() {
-            fetch('/game/memory')
-                .then(data => {
-                    this.game = data.text()
-                })
-            }
-        }
-        ">
-        <button x-on:click="fetchGame()">Fetch Game</button>
-        <div x-html="game"></div>
-        
-        </div>
     </div>`
