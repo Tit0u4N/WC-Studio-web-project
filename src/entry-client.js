39bee7962df232d1383b82529482c7c4a100ee08
samsoucoupe <samybendhiab@gmail.com>
2024-02-02 17:39:46 +0100

    feat: add RankBoardAlpineData with alpine #12

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,6 +1,7 @@
 import './style/style.scss'
 import Alpine from 'alpinejs'
 import {AccountAlpineData} from "./SSR/pages/Account.js";
+import {RankBoardAlpineData} from "./SSR/components/RankBoard.js";
 
 const getPageByURL = () => {
     const path = window.location.pathname.replace('/', '');
@@ -28,6 +29,7 @@ Alpine.store('pages', {
 });
 
 Alpine.data(AccountAlpineData.dataKey, AccountAlpineData.data);
+Alpine.data(RankBoardAlpineData.dataKey, RankBoardAlpineData.data);
 
 window.addEventListener('alpine:init', () => {
     setTimeout(() => {
