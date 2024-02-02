958f829e78f363871223fd2584f2b965acf40efb
theo mac <theo.lahmar1@gmail.com>
2024-02-02 16:36:43 +0100

    nice design, everything is too small, but everything is here

--- src/SSR/pages/Login.js
+++ src/SSR/pages/Login.js
@@ -9,110 +9,62 @@ const PageKey = 'login';
 export const Login =
 
 `
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 transform scale-200">
+
+    <!-- à enlever 
+  <div>
+    <h1 class="flex justify-center">HEADER COMPONENT</h1>
+  </div>
+    -->
+
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-10 w-auto" src="../../../public/assets/images/Logo-WC-Studio.png" alt="WC Studios">
+    <!-- <img class="mx-auto h-20 w-auto" src="../../../public/assets/images/Logo-WC-Studio.png" alt="WC Studios"> -->
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" action="#" method="POST">
       <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
+        <label for="username" class="block text-sm font-medium leading-6 text-white">Username : </label>
         <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input id="username" name="username" autocomplete="username" placeholder="Example : thatcoolusername6969" required 
+                class="block w-full rounded-2xl border-2 border-purple-300 py-1.5 text-gray-300 shadow-sm 
+                       sm:text-sm sm:leading-6 bg-[#4F356E]"
+          >
         </div>
       </div>
 
       <div>
         <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
+          <label for="password" class="block text-sm font-medium leading-6 text-white">Password : </label>
           <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            <a href="#" class="font-semibold text-purple-200 hover:text-white">Forgot password?</a>
           </div>
         </div>
-        <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        <div id="password-class" class="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" placeholder="Example : ****************" required 
+                class="block w-full rounded-2xl border-2 border-purple-300 py-1.5 text-gray-300 shadow-sm 
+                       sm:text-sm sm:leading-6 bg-[#4F356E]"
+          >
         </div>
       </div>
 
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      <!-- div + padding pour "simuler" un border en dégradé -->
+      <div id="div-under-login-button" class="rounded-3xl">
+        <button id="login-button" type="submit" class="flex w-full justify-center rounded-3xl bg-[#4F356E] px-3 py-1.5 
+                                     text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-100
+                                     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
+                                     focus-visible:outline-purple-200 hover:text-[#4F356E]"
+        >Sign in</button>
       </div>
     </form>
 
     <p class="mt-10 text-center text-sm text-gray-500">
       Not a member?
-      <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+      <!-- rediriger vers /register -->
+      <a href="#" class="font-semibold leading-6 text-purple-200 hover:text-white">Create an account</a>
     </p>
   </div>
 </div>
 
 `
-
-    
-/*
-    `<div x-data x-show="$store.pages.isShowing('login')">
-        <h1>LOGIN</h1>
-    </div>`
-*/
-    
-
-   /*
-
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
-      <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-      </div>
-
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-          </div>
-        </div>
-        <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-      </div>
-
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-      </div>
-    </form>
-
-    <p class="mt-10 text-center text-sm text-gray-500">
-      Not a member?
-      <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-    </p>
-  </div>
-</div>
-   */
-   /*
-    LayoutPage(
-        PageKey,
-        `
-            <h1>LOGIN</h1>
-        
-        `
-    )
-    */
