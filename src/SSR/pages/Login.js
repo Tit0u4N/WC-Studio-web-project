35a8e1d583f3dc8d749540652f8c035c06e8174c
theo mac <theo.lahmar1@gmail.com>
2024-02-07 23:51:57 +0100

    correctly sized login form elements

--- src/SSR/pages/Login.js
+++ src/SSR/pages/Login.js
@@ -9,62 +9,67 @@ const PageKey = 'login';
 export const Login =
 
 `
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 transform scale-200">
-
-    <!-- à enlever 
-  <div>
-    <h1 class="flex justify-center">HEADER COMPONENT</h1>
-  </div>
-    -->
-
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <!-- <img class="mx-auto h-20 w-auto" src="../../../public/assets/images/Logo-WC-Studio.png" alt="WC Studios"> -->
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+<div style="overflow: hidden; height: 70vh">
+    <div class="h-full">
+    
+        <!-- à enlever 
       <div>
-        <label for="username" class="block text-sm font-medium leading-6 text-white">Username : </label>
-        <div class="mt-2">
-          <input id="username" name="username" autocomplete="username" placeholder="Example : thatcoolusername6969" required 
-                class="block w-full rounded-2xl border-2 border-purple-300 py-1.5 text-gray-300 shadow-sm 
-                       sm:text-sm sm:leading-6 bg-[#4F356E]"
-          >
-        </div>
+        <h1 class="flex justify-center">HEADER COMPONENT</h1>
       </div>
-
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-white">Password : </label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-purple-200 hover:text-white">Forgot password?</a>
+        -->
+    
+      <div class="mx-auto w-full">
+        <!-- <img class="mx-auto h-20 w-auto" src="../../../public/assets/images/Logo-WC-Studio.png" alt="WC Studios"> -->
+        <h2 class="mt-10 text-center text-7xl font-bold leading-9 tracking-tight text-white">SIGN IN TO YOUR ACCOUNT</h2>
+      </div>
+    
+      <div class="mt-10 mx-auto w-1/3">
+        <form class="space-y-6" action="#" method="POST">
+          <div>
+            <label for="username" class="block text-xl font-medium leading-6 text-white">Username : </label>
+            <div class="mt-2">
+              <input id="username" name="username" autocomplete="username" placeholder="Example : thatcoolusername6969" required 
+                    class="block w-full rounded-2xl border-2 border-purple-300 px-3.5 py-2 text-gray-300 shadow-xl 
+                           text-xl leading-6 bg-[#4F356E]"
+              >
+            </div>
           </div>
-        </div>
-        <div id="password-class" class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" placeholder="Example : ****************" required 
-                class="block w-full rounded-2xl border-2 border-purple-300 py-1.5 text-gray-300 shadow-sm 
-                       sm:text-sm sm:leading-6 bg-[#4F356E]"
-          >
-        </div>
+    
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-xl font-medium leading-6 text-white">Password : </label>
+              <div class="text-lg">
+                <a href="#" class="font-semibold text-purple-200 hover:text-white">Forgot password?</a>
+              </div>
+            </div>
+            <div id="password-class" class="mt-2">
+              <input id="password" name="password" type="password" autocomplete="current-password" placeholder="Example : ****************" required 
+                    class="block w-full rounded-2xl border-2 border-purple-300 px-3.5 py-2 text-gray-300 shadow-xl
+                           text-xl leading-6 bg-[#4F356E]"
+              >
+            </div>
+          </div>
+    
+          <!-- div pour centrer le bouton de login -->
+          <div class="flex justify-center w-2/3 mx-auto">
+              <!-- div + padding pour "simuler" un border en dégradé -->
+              <div id="div-under-login-button" class="rounded-3xl">
+                <button id="login-button" type="submit" class="flex w-full justify-center rounded-3xl bg-[#4F356E] py-2.5
+                                             text-2xl font-semibold leading-6 text-white shadow-xl hover:bg-purple-100
+                                             focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
+                                             focus-visible:outline-purple-200 hover:text-[#4F356E]"
+                >Sign in</button>
+              </div>
+          </div>
+        </form>
+    
+        <p class="mt-10 text-center text-lg text-gray-500">
+          Not a member?
+          <!-- rediriger vers /register -->
+          <a href="#" class="font-semibold leading-6 text-purple-200 hover:text-white">Create an account</a>
+        </p>
       </div>
-
-      <!-- div + padding pour "simuler" un border en dégradé -->
-      <div id="div-under-login-button" class="rounded-3xl">
-        <button id="login-button" type="submit" class="flex w-full justify-center rounded-3xl bg-[#4F356E] px-3 py-1.5 
-                                     text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-100
-                                     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
-                                     focus-visible:outline-purple-200 hover:text-[#4F356E]"
-        >Sign in</button>
-      </div>
-    </form>
-
-    <p class="mt-10 text-center text-sm text-gray-500">
-      Not a member?
-      <!-- rediriger vers /register -->
-      <a href="#" class="font-semibold leading-6 text-purple-200 hover:text-white">Create an account</a>
-    </p>
-  </div>
+    </div>
 </div>
 
 `
