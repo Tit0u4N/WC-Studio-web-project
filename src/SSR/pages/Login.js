2e94ac2b4a0bbb2d97cdfd7c5517bd0098aeba47
Titouan Lacombe--Fabre <titouan.lacombe---fabre@etu.univ-cotedazur.fr>
2024-02-09 11:38:30 +0100

    feat: redirection en fonction de si un joueur est connecté ou non #31

--- src/SSR/pages/Login.js
+++ src/SSR/pages/Login.js
@@ -1,4 +1,5 @@
 import { LayoutPage } from "../layouts/LayoutPage";
+import {UserData} from "../../js/global/UserData.js";
 
 const PageKey = 'login';
 
@@ -10,11 +11,11 @@ export const Login = LayoutPage(PageKey,
       </div>
     
       <div class="mt-10 mx-auto w-1/3">
-        <form class="space-y-10">
+        <form x-data="${PageKey}" x-on:submit.prevent="login()" class="space-y-10">
           <div>
             <label for="username" class="block text-xl font-medium leading-6 text-white">Username : </label>
             <div class="mt-2">
-              <input id="username" name="username" autocomplete="username" placeholder="thatcoolusername6969" required 
+              <input x-model="username" id="username" name="username" autocomplete="username" placeholder="thatcoolusername6969" required 
                     class="block w-full rounded-2xl border-2 border-purple-300 px-3.5 py-2 text-gray-300 shadow-xl 
                            text-xl leading-6 bg-[#4F356E]"
               >
@@ -26,7 +27,7 @@ export const Login = LayoutPage(PageKey,
               <label for="password" class="block text-xl font-medium leading-6 text-white">Password : </label>
             </div>
             <div id="password-class" class="mt-2">
-              <input id="password" name="password" type="password" autocomplete="current-password" placeholder="****************" required 
+              <input x-model="password" id="password" name="password" type="password" autocomplete="current-password" placeholder="****************" required 
                     class="block w-full rounded-2xl border-2 border-purple-300 px-3.5 py-2 text-gray-300 shadow-xl
                            text-xl leading-6 bg-[#4F356E]"
               >
@@ -46,4 +47,24 @@ export const Login = LayoutPage(PageKey,
       </div>
     </div>
 `);
+
+export const LoginAlpineData = {
+    dataKey : PageKey,
+    data : () => ({
+        username: '',
+        password: '',
+        login() {
+            const user = UserData.getExistingUserData();
+
+            if (user.isNewUserData()) {
+                user.setEmail(this.username)
+                user.setPassword(this.password)
+                user.save()
+            }
+
+            this.$store.pages.set('home')
+        }
+    })
+}
+
 
--- src/entry-client.js
+++ src/entry-client.js
@@ -1,6 +1,8 @@
 import './style/style.scss'
 import Alpine from 'alpinejs'
 import {AccountAlpineData} from "./SSR/pages/Account.js";
+import {LoginAlpineData} from "./SSR/pages/Login.js";
+import {UserData} from "./js/global/UserData.js";
 
 const getPageByURL = () => {
     const path = window.location.pathname.replace('/', '');
@@ -22,14 +24,21 @@ Alpine.store('pages', {
         return this.showing === page;
     },
     set(page) {
+        if (UserData.getExistingUserData().isNewUserData() && (page === 'account' || page === 'shop')) {
+            return this.showing = 'login';
+        }
         if (this.showing === page) return;
         this.showing = page;
     },
 });
 
+Alpine.data(LoginAlpineData.dataKey, LoginAlpineData.data);
 Alpine.data(AccountAlpineData.dataKey, AccountAlpineData.data);
 
 window.addEventListener('alpine:init', () => {
+    if (UserData.getExistingUserData().isNewUserData()) {
+        Alpine.store('pages').set('login');
+    }
     setTimeout(() => {
         document.getElementById("pageLoader").classList.add("!hidden")
     }, 1000);
