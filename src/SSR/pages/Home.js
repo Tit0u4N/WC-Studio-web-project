b787d8e5db0d0d379dc5361b978cf28128edeb53
theo mac <theo.lahmar1@gmail.com>
2024-02-09 11:37:56 +0100

    MAJOR bugfix (fixed the whole app's layout)

--- src/SSR/pages/Home.js
+++ src/SSR/pages/Home.js
@@ -2,19 +2,12 @@ import {Success} from "../components/Success.js";
 
 export const Home =
     `
-    <!-- main homepage div -->
-    <!-- style="display: flex; flex-direction: column; justify-content: space-evenly; align-items: center; height:70vh; --> 
-    <div id="homePageContainer" class="flex flex-col justify-evenly items-center">
-    
-        <!-- homepage's content header (games preview) + other components  -->
-        <div x-data x-show="$store.pages.isShowing('home')">
-            <h1>HOME</h1>
-        </div> 
-        
+    <div x-data x-show="$store.pages.isShowing('home')" id="homePageContainer" class="flex flex-col justify-evenly items-center">
+        <h1>HOME</h1>
         <!-- successes list -->
         <div>
             <div> ${Success("success name", "game name", "69%", "miage")}</div>
         </div>
-        
-    </div>
+    </div> 
+
     `
