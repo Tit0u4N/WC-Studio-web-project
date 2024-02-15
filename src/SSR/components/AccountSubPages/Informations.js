59a0aa7ccce940d593ac15ef213eda27ad94655f
theo mac <theo.lahmar1@gmail.com>
2024-02-15 11:03:30 +0100

    text width is now responsive, converted some css to tailwindcss

--- src/SSR/components/AccountSubPages/Informations.js
+++ src/SSR/components/AccountSubPages/Informations.js
@@ -5,8 +5,8 @@ export const Informations = (id = "Informations") => {
         <div class="flex flex-col justify-between h-full pb-8 pt-4">
         
             <div class="flex justify-start items-center">
-                <h3>Nom et Prénom</h3>
-                <div id="aestheticNeon"></div>
+                <h3 class="w-[20%]">Nom et Prénom</h3>
+                <div class="w-3/4 h-[2px] rounded ml-4" id="aestheticNeon"></div>
             </div>
             
             ${FormField("Username", "Exemple : thatcoolusername7979")}
@@ -14,8 +14,8 @@ export const Informations = (id = "Informations") => {
             ${FormField("Name", "Exemple : coolName")}
     
             <div class="flex justify-start items-center pt-6">
-                <h3>Mot de passe </h3>
-                <div id="aestheticNeon"></div>
+                <h3 class="w-[20%]">Mot de passe </h3>
+                <div class="w-3/4 h-[2px] rounded ml-4" id="aestheticNeon"></div>
             </div>
             
             ${FormField("Password", "****************")}
