a44a267ac1e1eef8ba9864fd815bd25274e9c095
theo mac <theo.lahmar1@gmail.com>
2024-03-04 12:54:32 +0100

    first rankboard workaround (not fully fixing)

--- src/SSR/pages/Home.js
+++ src/SSR/pages/Home.js
@@ -4,8 +4,15 @@ import {Success} from "../components/Success.js";
 export const Home =
     `
     <div x-data x-show="$store.pages.isShowing('home')" id="homePageContainer" class="flex flex-col justify-evenly items-center">
-        <h1>HOME</h1>
-        ${RankBoard}
+        <!-- barre esthétique à ajouter ici --> 
+        
+        <!-- ajout de la height du header + son padding en y (170 + 16 = 186px) -->
+        <div class="flex justify-around pt-[186px] w-full">
+            <div>
+            <!-- ajouter les 4 cards (grid 2x2 : chaque élément à 460px * 276px) -->
+            </div>
+            ${RankBoard}
+        </div>
         ${Success()}
     </div> 
 
