9eac7a668b27a15e839dd2ba28b78840e8e185c8
theo <theo.lahmar1@gmail.com>
2024-04-13 18:08:24 +0200

    volume slider's nb of steps increased from 10 to 100

--- src/SSR/components/SoundSetter.js
+++ src/SSR/components/SoundSetter.js
@@ -2,7 +2,7 @@ export const SoundSetter = (title, musicWavFileUrl) => {
     return `
     <div class="flex flex-row items-center gap-6">
         <p class="pt-1">${title}</p> 
-        <input type="range" min="0" max="1.0" step="0.1" value="0.5" x-data="$store.music" x-on:change="setVolume($event.target.value)"
+        <input type="range" min="0" max="1.0" step="0.01" value="0.50" x-data="$store.music" x-on:change="setVolume($event.target.value)"
                class="range accent-[#EDC1FE]">
     </div>
     `
