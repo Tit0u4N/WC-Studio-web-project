13616c4ade2035d9ecaf121f7519294dbfaac99e
theo <theo.lahmar1@gmail.com>
2024-04-15 21:11:01 +0200

    v2 higherbutton : added a blur to the border + implemented in settings-informations page (save password)

--- src/SSR/components/AccountSubPages/Informations.js
+++ src/SSR/components/AccountSubPages/Informations.js
@@ -1,10 +1,13 @@
 import {FormField} from "../FormField.js";
 import {NeonHeaderBar} from "../NeonHeaderBar.js";
 import {ButtonHover} from "../ButtonHover.js";
+import { HigherButton } from "../HigherButton.js";
+
+const PageKey = "account/informations";
 
 export const Informations = (id = "Informations") => {
     return `
-        <div class="flex flex-col justify-between h-full pb-8 pt-4">
+        <div class="flex flex-col justify-between h-full py-4">
         
             ${NeonHeaderBar("Your Username")}
             
@@ -19,10 +22,20 @@ export const Informations = (id = "Informations") => {
             ${FormField("Password", "****************")}
             ${FormField("Confirm Password", "****************")}
 
-            <div class="">
-                <!-- ${ButtonHover('Save', {alpineProps: 'x-on:click="nomFonction"'})} -->
+            <div class="pt-4">
+                ${HigherButton('savePasswordButton', 'x-on:click="savePassword()"', 'Save Changes')}
             </div>
             
         </div>
     `
+}
+
+export const InformationsAlpineData = {
+    dataKey : PageKey,
+    data : () => ({
+        savePassword() {
+            // tester si le nouveau password est correct (ou si la modif est acceptable)
+            console.log("save password button has been clicked !");
+        }
+    })
 }
